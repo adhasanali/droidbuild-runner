f631ced4816287b1119591c46d2276cf2f00b24d
adhasanali <268511121+adhasanali@users.noreply.github.com>
2026-05-21 22:44:01 +0800

    Update app/src/main/java/com/build/smartcalc/MainActivity.java

---
+++ app/src/main/java/com/build/smartcalc/MainActivity.java
@@ -0,0 +1,19 @@
+package com.build.smartcalc;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
